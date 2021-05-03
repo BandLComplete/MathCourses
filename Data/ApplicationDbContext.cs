c691abba5239dfb8793e7ce6a5b208c3e0c7bccd
BandLComplete <boroviki2000@mail.ru>
2021-05-03 12:41:13 +0500

    Добавил строку подключения

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace InProcess.Data
         
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
+			optionsBuilder.UseSqlServer(@"Data Source=mathcoursesserver.database.windows.net;Initial Catalog=MathCoursesDB;User ID=bandl;Password=DayZstandalone200;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 			//optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 			//optionsBuilder.UseSqlServer(@"Data Source=MathCourses.mssql.somee.com;Initial Catalog=MathCourses;User ID=BandLComplete_SQLLogin_1;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 		}
